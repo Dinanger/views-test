780bae415d9d5572f79511742d9835be72ff1f80
dina <dinanyel01@hotmail.com>
2026-02-13 02:57:04 -0400

    odoo completo, solo ajustar

--- addons/UNI/models/classroom.py
+++ addons/UNI/models/classroom.py
@@ -5,7 +5,7 @@ class classroom(models.Model):
     _name = "classroom.university"
     _description = "Classroom" 
 
-    numid = fields.Integer(string="numid", required=True)
+    num_id = fields.Integer(string="numid", required=True)
     
     teacher = fields.Many2one(
         "teacher.university",
